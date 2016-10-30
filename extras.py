0e2cd05c8a7b876fedcb80e0ee26e9ae1c21f607
nevrrmind <td-xtc@gmx.de>
2016-10-30 23:00:38 +0100

    Create extras.py

---
+++ extras.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+warnlist =["7500","7600","7700","0020","0037"]
+sqkinfo = {
+"7500":"ENTFÜHRUNG",
+"7600":"FUNKAUSFALL",
+"7700":"NOTFALL",
+"7000":"VFR-zivil",
+"0037":"BPO mit Restlicht",
+"1000":"IFR",
+"0020":"Hubschrauber-Rettungsflüge",
+}
